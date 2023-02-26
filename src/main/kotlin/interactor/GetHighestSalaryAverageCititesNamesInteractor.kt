f56063c2e317bda35a7f20c2c58137b7f76c5c8b
Ali Gamal <alialashwall14@gmail.com>
2023-02-26 17:52:02 +0200

    class after removing condition of data quality

--- src/main/kotlin/interactor/GetHighestSalaryAverageCititesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetHighestSalaryAverageCititesNamesInteractor.kt
@@ -6,17 +6,16 @@ class GetHighestSalaryAverageCititesNamesInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(limit: Int): List<String> {
+    fun execute(limit: Int): List<CityEntity> {
         return dataSource
             .getAllCitiesData()
             .filter(::excludeNullSalariesAndLowQualityData)
             .sortedByDescending { it.averageMonthlyNetSalaryAfterTax }
             .take(limit)
-            .map { it.cityName }
     }
 
     private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
-        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+        return city.averageMonthlyNetSalaryAfterTax != null
     }
 
 }
